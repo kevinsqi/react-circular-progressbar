768d8c44fb21f8adfaf0ea40148968158013d85c
Kevin Qi <iqnivek@gmail.com>
2018-06-24 13:21:47 -0700

    remove tests for removed props

--- test/components/CircularProgressbar.spec.js
+++ test/components/CircularProgressbar.spec.js
@@ -28,48 +28,6 @@ describe('CircularProgressbar props', () => {
     assert.equal(2, wrapper.find('.CircularProgressbar-path').prop('strokeWidth'));
   });
 
-  it('classForPercentage', () => {
-    function classForPercentage(percentage) {
-      return percentage < 100 ? 'incomplete' : 'done';
-    }
-    const bar1 = shallow(
-      <CircularProgressbar
-        percentage={50}
-        classForPercentage={classForPercentage}
-      />
-    );
-    assert.include(bar1.find('.CircularProgressbar').prop('className'), 'incomplete', 'should have correct class depending on percentage');
-
-    const bar2 = shallow(
-      <CircularProgressbar
-        percentage={100}
-        classForPercentage={classForPercentage}
-      />
-    );
-    assert.include(bar2.find('.CircularProgressbar').prop('className'), 'done', 'should have correct class depending on percentage');
-  });
-
-  it('textForPercentage', () => {
-    function textForPercentage(percentage) {
-      return percentage < 50 ? `meh ${percentage}` : `yey ${percentage}`;
-    }
-    const bar1 = shallow(
-      <CircularProgressbar
-        percentage={25}
-        textForPercentage={textForPercentage}
-      />
-    );
-    assert.equal('meh 25', bar1.find('.CircularProgressbar-text').prop('children'));
-
-    const bar2 = shallow(
-      <CircularProgressbar
-        percentage={80}
-        textForPercentage={textForPercentage}
-      />
-    );
-    assert.equal('yey 80', bar2.find('.CircularProgressbar-text').prop('children'));
-  });
-
   it('className', () => {
     const wrapper = shallow(
       <CircularProgressbar
@@ -116,9 +74,11 @@ describe('CircularProgressbar props', () => {
   });
 
   it('styles', () => {
+    const percentage = 50;
     const wrapper = shallow(
       <CircularProgressbar
-        percentage={50}
+        percentage={percentage}
+        text={`${percentage}%`}
         background
         styles={{
           root: { stroke: '#000000' },
