bb189338965cdae105ee140999248e21cfb1c3e6
Apostolis Economou <apostolis.economou@gmail.com>
2019-01-29 00:34:11 +0200

    Add support for centered text in IE Add detectIE() to conditionally add dy prop to <text> for center alignment.

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,50 @@ const FULL_RADIUS = 50;
 const CENTER_X = 50;
 const CENTER_Y = 50;
 
+/**
+ * @see {@link https://codepen.io/gapcode/pen/vEJNZN}
+ * @see {@link https://stackoverflow.com/a/44929931/5420369}
+ */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  // Test values; Uncomment to check result …
+
+  // IE 10
+  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+  // IE 11
+  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+  // Edge 12 (Spartan)
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+  // Edge 13
+  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
+
 class CircularProgressbar extends React.Component {
   constructor(props) {
     super(props);
@@ -139,6 +183,7 @@ class CircularProgressbar extends React.Component {
               style={styles.text}
               x={CENTER_X}
               y={CENTER_Y}
+              dy={detectIE() ? "0.3em" : undefined }
             >
               {text}
             </text>
