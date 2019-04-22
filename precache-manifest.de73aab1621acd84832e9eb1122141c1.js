a64f0000b4c081b80df874ec4f9484b8ae6879ad
Kevin Qi <kqi@amino.com>
2019-04-21 17:44:56 -0700

    Updates

---
+++ precache-manifest.de73aab1621acd84832e9eb1122141c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe43bacfc5f0c12b970",
+    "url": "/static/css/main.c5efd66a.chunk.css"
+  },
+  {
+    "revision": "9fe43bacfc5f0c12b970",
+    "url": "/static/js/main.01396acc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39ee7bfb8dd34291b3db",
+    "url": "/static/js/2.78985d59.chunk.js"
+  },
+  {
+    "revision": "6b4834c29b23ece0a8a99d6978da2a8c",
+    "url": "/index.html"
+  }
+];
