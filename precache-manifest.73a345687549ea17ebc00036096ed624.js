2882a8711ff355cba004be14f9f053f26f0de7ed
Kevin Qi <kqi@amino.com>
2019-04-21 17:52:33 -0700

    Updates

---
+++ precache-manifest.73a345687549ea17ebc00036096ed624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cdb4ece0cbb9d293157",
+    "url": "/react-circular-progressbar/static/css/main.c5efd66a.chunk.css"
+  },
+  {
+    "revision": "7cdb4ece0cbb9d293157",
+    "url": "/react-circular-progressbar/static/js/main.3dc6ef97.chunk.js"
+  },
+  {
+    "revision": "84b3e6b81f1edb806d20",
+    "url": "/react-circular-progressbar/static/js/runtime~main.3915c496.js"
+  },
+  {
+    "revision": "39ee7bfb8dd34291b3db",
+    "url": "/react-circular-progressbar/static/js/2.78985d59.chunk.js"
+  },
+  {
+    "revision": "bc4983a6ee0fdd8f864d696e4fe83501",
+    "url": "/react-circular-progressbar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de73aab1621acd84832e9eb1122141c1.js"
+  "/react-circular-progressbar/precache-manifest.73a345687549ea17ebc00036096ed624.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-circular-progressbar/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
