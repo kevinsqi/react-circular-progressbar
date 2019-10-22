04a195f53a2dd4ba02ea0051ac1e159f6f9d97dc
Kevin Qi <iqnivek@gmail.com>
2019-10-22 15:53:48 -0700

    Updates

---
+++ precache-manifest.e34c9be77fd4284824e5bb27a4900d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bf472b943d66acb1aa77e75b19aeae",
+    "url": "/react-circular-progressbar/index.html"
+  },
+  {
+    "revision": "2b13c5300cb1fadbe1f4",
+    "url": "/react-circular-progressbar/static/css/main.ba436296.chunk.css"
+  },
+  {
+    "revision": "5f3acb9312e17d74d26c",
+    "url": "/react-circular-progressbar/static/js/2.0a7b7f8e.chunk.js"
+  },
+  {
+    "revision": "2b13c5300cb1fadbe1f4",
+    "url": "/react-circular-progressbar/static/js/main.28c7dc36.chunk.js"
+  },
+  {
+    "revision": "84b3e6b81f1edb806d20",
+    "url": "/react-circular-progressbar/static/js/runtime~main.3915c496.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-circular-progressbar/precache-manifest.4e9940b99e45cf4ba05f04a80c93f19f.js"
+  "/react-circular-progressbar/precache-manifest.e34c9be77fd4284824e5bb27a4900d5c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-circular-progressbar/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-circular-progressbar/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
